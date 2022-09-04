c97cf8c34c0cca342fbc9e2b61ec9d95e56864ca
DicanGuo <87957743+DicanGuo@users.noreply.github.com>
2022-09-04 17:43:51 +1000

    session editable

--- front/src/app/profile/profile.component.ts
+++ front/src/app/profile/profile.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
+
 export class ProfileComponent implements OnInit {
+  // user = {'username': '', 'email': '', 'birthdate': '', 'age': '', 'valid': false}
+  username = '';
+  birthdate = '';
+  age = '';
+  email = '';
+  valid = '';
+  // username = sessionStorage.getItem('username');
+  // birthdate = sessionStorage.getItem('birthdate');
+  // age = sessionStorage.getItem('age');
+  // email = sessionStorage.getItem('email');
+  // valid = sessionStorage.getItem('valid');
+  // user = {'username': this.username, 'email': this.email, 'birthdate': this.birthdate, 'age': this.age, 'valid': this.valid}
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router:Router) { 
+    // if (!(sessionStorage.getItem('valid')=="true")){
+    //   alert("login please");
+    //   this.router.navigateByUrl("/login");
+    // }
+    this.username = sessionStorage.getItem('username')!;
+    this.birthdate = sessionStorage.getItem('birthdate')!;
+    this.age = sessionStorage.getItem('age')!;
+    this.email = sessionStorage.getItem('email')!;
+    console.log(this.username);
+    console.log(this.birthdate);
+    console.log(this.age);
+    console.log(this.email);
 
+
+  }
   ngOnInit(): void {
   }
+  title = "Profile"
 
+  logout(){
+    sessionStorage.clear();
+    console.log(sessionStorage);
+    this.router.navigateByUrl('/');
+  }
+  save(){
+    console.log(sessionStorage);
+
+    // sessionStorage.setItem('username', this.user.username);
+    sessionStorage.setItem('age', this.age);
+    console.log(sessionStorage)
+    //this.router.navigateByUrl('/account/' + this.username);
+
+  }
 }
